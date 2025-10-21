584112985a6d371c500588327c0c7dfbc2073c1a
mlich <mlichmatus0@gmail.com>
2025-10-21 18:01:12 +0200

    doma cosi

--- cisla/views.py
+++ cisla/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 a=0
 b=0
-apar=""
-bpar=""
+
+
 def index(request):
+
+    apar = ""
+    bpar = ""
+    vysledok_prvocislo=""
+    vysledok_prvocislob=""
+
+
     if request.method == "GET":
             vysledok = 0
     if request.method == "POST":
-        a = (request.POST["cislo1"])
-        b = (request.POST["cislo2"])
+        a = int((request.POST["cislo1"]))
+        b = int((request.POST["cislo2"]))
         
         if a>b:
              vysledok="cislo a je vacsie"
@@ -30,6 +37,33 @@ def index(request):
         else:
             bpar = "cislo B je neparne"
          
+        if a <= 1:
+            prvocislo = False
+        else:
+            prvocislo = True
+            for i in range(2, a):
+                if a % i == 0:
+                    prvocislo = False
+                    break
+
+        if prvocislo:
+            vysledok_prvocislo = "cislo a je prvočíslo"
+        else:
+            vysledok_prvocislo = "cislo a nie je prvočíslo"
 
 
-    return render(request, 'cisla/index.html', {"vysledok": vysledok, "apar":apar ,"bpar":bpar})
+        if b <= 1:
+            prvocislob = False
+        else:
+            prvocislob = True
+            for i in range(2, b):
+                if b % i == 0:
+                    prvocislob = False
+                    break
+
+        if prvocislob:
+            vysledok_prvocislob = "cislo b je prvočíslo"
+        else:
+            vysledok_prvocislob = "cislo b nie je prvočíslo"
+
+    return render(request, 'cisla/index.html', {"vysledok": vysledok,"apar": apar,"bpar": bpar,"vysledok_prvocislo":vysledok_prvocislo,"vysledok_prvocislob":vysledok_prvocislob})
